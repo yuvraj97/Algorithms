a74583b465126fa6624cc3fde548aa1f3eaa4907
Yuvraj Garg <yuvraj97garg@gmail.com>
2018-12-21 02:45:12 +0530

    Updated VAM method now its more efficient.

--- Transportation Problum/TransportationProblum.py
+++ Transportation Problum/TransportationProblum.py
@@ -3,12 +3,12 @@
 Created on Tue Dec 18 18:29:09 2018
 @author: Yuvraj
 
-Updation: added properties of VAM
-Updated on Wed Dec 19 15:09:55 2018
 """
 
 import numpy as np
 import sys
+import time
+start = time.time()
 
 def NorthwestCornerCell(supply,demand,cost):
     rows = cost.shape[0]
@@ -91,7 +91,8 @@ def LeastCostCell(supply,demand,cost):
         costCopy[mini,minj]=Infinity
     return output,total_cost
 
-def VogalsApproximation(supply,demand,cost):
+#This VAM method is too slow an updated method is given below
+def VogalsApproximationV1(supply,demand,cost):
     def updateCostFalse(cost):
         costCopy = []
         for i in range(len(cost)):
@@ -139,6 +140,7 @@ def VogalsApproximation(supply,demand,cost):
             return -a
     def getMin(array):
         min = np.inf
+        position = None
         for i in range(len(array)):
             if(array[i][1] == True):
                 continue
@@ -148,11 +150,13 @@ def VogalsApproximation(supply,demand,cost):
         return position
     
     costCopy = updateCostFalse(cost)
+    supplyCopy = supply.copy()
+    demandCopy = demand.copy()
     rows = cost.shape[0]
     column = cost.shape[1]
     output = np.zeros(cost.shape)
     total_cost = 0
-    while(checkNonZero(supply) and checkNonZero(demand)):
+    while(checkNonZero(supplyCopy) and checkNonZero(demandCopy)):
         row_diff_position = []
         col_diff_position = []
         for i in range(rows):
@@ -182,27 +186,178 @@ def VogalsApproximation(supply,demand,cost):
             getLeastFrom = costCopy[:,max_i]
             leastPosition = getMin(getLeastFrom)
             i,j = leastPosition, max_i
+        if(leastPosition == None):
+            continue
         costCopy[i][j][1] = True        
-        if(supply[i] == 0):
+        if(supplyCopy[i] == 0):
             continue
-        if(demand[j] == 0):
+        if(demandCopy[j] == 0):
             continue
-        if(demand[j] < supply[i]):
-            total_cost += demand[j] * cost[i,j]
-            supply[i] -= demand[j]
-            output[i,j] = demand[j]
-            demand[j] = 0
-        elif(supply[i] < demand[j]):
-            total_cost += supply[i] * cost[i,j]
-            demand[j] -= supply[i]
-            output[i,j] = supply[i]
-            supply[i] = 0
+        if(demandCopy[j] < supplyCopy[i]):
+            total_cost += demandCopy[j] * cost[i,j]
+            supplyCopy[i] -= demandCopy[j]
+            output[i,j] = demandCopy[j]
+            demandCopy[j] = 0
+        elif(supplyCopy[i] < demandCopy[j]):
+            total_cost += supplyCopy[i] * cost[i,j]
+            demandCopy[j] -= supplyCopy[i]
+            output[i,j] = supplyCopy[i]
+            supplyCopy[i] = 0
         else:
-            total_cost += supply[i] * cost[i,j]
-            output[i,j] = supply[i]
-            supply[i] = demand[j] = 0
+            total_cost += supplyCopy[i] * cost[i,j]
+            output[i,j] = supplyCopy[i]
+            supplyCopy[i] = demandCopy[j] = 0
     return output,total_cost    
 
+def VogalsApproximationV2(supply,demand,cost):
+    def get2MinRow(array):     # I/P array will bew 2D where 2nd dimention is used to check if the element is visited or not
+        min1 = np.inf
+        min2 = np.inf
+        min1j = min2j = -1
+        for i in range(len(array)):
+            if(array[i] <= min1):
+                min1 = array[i]
+                min1j = i
+        for i in range(len(array)):
+            if(array[i] <= min2 and i != min1j):
+                min2 = array[i]
+                min2j = i
+        return min1j, min2j
+    def get2MinColumn(matrix,col):     # I/P array will bew 2D where 2nd dimention is used to check if the element is visited or not
+        min1 = np.inf
+        min2 = np.inf
+        min1j = min2j = -1
+        noRows = matrix.shape[0]
+        for i in range(noRows):
+            if(matrix[i,col] <= min1):
+                min1 = matrix[i,col]
+                min1j = i
+        for i in range(noRows):
+            if(matrix[i,col] <= min2 and i != min1j):
+                min2 = matrix[i,col]
+                min2j = i
+        return min1j, min2j
+    def maxOfRowColDiff(row_diff,col_diff):
+        isRowMax = True
+        max = -np.inf
+        position = -1
+        for i in range(len(row_diff)):
+            if(row_diff[i]  > max):
+                max = row_diff[i]
+                position = i
+                isRowMax = True
+        for i in range(len(col_diff)):
+            if(col_diff[i]  > max):
+                max = col_diff[i]
+                position = i
+                isRowMax = False
+        return position, isRowMax       # isRowMax is used to indicate wather the max is from row_diff or col_diff
+    def abs(a):
+        if(a>0):
+            return a
+        else:
+            return -a
+    def getMinRow(array):
+        min = np.inf
+        position = -1
+        for i in range(len(array)):
+            if(array[i] < min):
+                min = array[i]
+                position = i
+        return position
+    def getMinColumn(matrix,col):
+        min = np.inf
+        position = -1
+        noRows = matrix.shape[0]
+        for i in range(noRows):
+            if(matrix[i,col] < min):
+                min = matrix[i,col]
+                position = i
+        return position
+    
+    costCopy = cost.copy()
+    supplyCopy = supply.copy()
+    demandCopy = demand.copy()
+    rows = cost.shape[0]
+    column = cost.shape[1]
+    output = np.zeros(cost.shape)
+    total_cost = 0
+    while(checkNonZero(supplyCopy) and checkNonZero(demandCopy)):
+        row_diff_position = []
+        col_diff_position = []
+        for i in range(rows):
+            if(supplyCopy[i] != 0):
+                minr1 ,minr2 = get2MinRow(costCopy[i])
+                row_diff_position.append([(i,minr1),(i,minr2)])
+            else:                                           # supplyCopy == 0 mean that whole row[i] is infinity
+                row_diff_position.append([(i,0),(i,0)])
+        for i in range(column):
+            if(demandCopy[i] != 0):
+                minc1, minc2 = get2MinColumn(costCopy,i)
+                col_diff_position.append([(minc1,i),(minc2,i)])
+            else:                                           # demandCopy == 0 mean that whole column[i] is infinity
+                col_diff_position.append([(0,i),(0,i)])
+        row_diff_arrray = []    
+        for i in range(len(row_diff_position)):
+            min1, min2 = costCopy[row_diff_position[i][0]], costCopy[row_diff_position[i][1]]
+            if(min1 == np.inf and min2 == np.inf):
+                row_diff_arrray.append(-np.inf)
+            elif(min1 != np.inf and min2 == np.inf):
+                row_diff_arrray.append(abs(min1))
+            else:
+                row_diff_arrray.append(abs(min1 - min2))
+        col_diff_arrray = []
+        for i in range(len(col_diff_position)):
+            min1, min2 = costCopy[col_diff_position[i][0]], costCopy[col_diff_position[i][1]]
+            if(min1 == np.inf and min2 == np.inf):
+                col_diff_arrray.append(-np.inf)
+            elif(min1 != np.inf and min2 == np.inf):
+                col_diff_arrray.append(abs(min1))
+            else:
+                col_diff_arrray.append(abs(min1 - min2))
+        max_i, isRow = maxOfRowColDiff(row_diff_arrray,col_diff_arrray)
+        if(isRow):
+            leastPosition = getMinRow(costCopy[max_i])
+            i,j = max_i, leastPosition
+        else:
+            leastPosition = getMinColumn(costCopy, max_i)
+            i,j = leastPosition, max_i
+        if(leastPosition == -1):
+            continue
+        costCopy[i,j] = np.inf
+        if(supplyCopy[i] == 0):
+            continue
+        if(demandCopy[j] == 0):
+            continue
+        if(demandCopy[j] < supplyCopy[i]):
+            total_cost += demandCopy[j] * cost[i,j]
+            supplyCopy[i] -= demandCopy[j]
+            output[i,j] = demandCopy[j]
+            demandCopy[j] = 0
+            deleteCol = 1
+        elif(supplyCopy[i] < demandCopy[j]):
+            total_cost += supplyCopy[i] * cost[i,j]
+            demandCopy[j] -= supplyCopy[i]
+            output[i,j] = supplyCopy[i]
+            supplyCopy[i] = 0
+            deleteCol = 0
+        else:
+            total_cost += supplyCopy[i] * cost[i,j]
+            output[i,j] = supplyCopy[i]
+            supplyCopy[i] = demandCopy[j] = 0
+            deleteCol = 2
+        if(deleteCol == 1):          # delete column
+            for ittr in range(rows):
+                costCopy[ittr, j] = np.inf
+        elif(deleteCol == 0):        # delete row
+            for ittr in range(column):
+                costCopy[i, ittr] = np.inf
+        elif(deleteCol == 2):        # delete row and column
+            for ittr in range(rows):
+                costCopy[ittr, j] = np.inf
+            for ittr in range(column):
+                costCopy[i, ittr] = np.inf        
+    return output,total_cost    
 
 def read(filename):
     print("Loading Dataset...")
@@ -240,12 +395,33 @@ def printMatrix(matrix):
         for j in range(len(matrix[i])):
             print(matrix[i][j],end="\t")
         print()
-
+def checkAnswer(supply, demand, output):
+    rows = output.shape[0]
+    columns= output.shape[1]
+    #Checking Column validity(Demand)
+    for j in range(columns):
+        jthSum =0
+        for i in range(rows):
+            jthSum += output[i][j]
+        if(jthSum != demand[j]):
+            return False
+    #Checking Rows Valdity (Supply)
+    for i in range(rows):
+        ithRow = 0
+        for j in range(columns):
+            ithRow += output[i][j]
+        if(ithRow != supply[i]):
+            return False
+    return True
+            
+        
+        
 def main():
     if len(sys.argv) != 3:
         print("Usage: ",sys.argv[0],"  argument(NWC/LCC/VAM)    Dataset.csv");
         exit(0)
     supply, demand, cost = read(sys.argv[2])
+    cost = cost.astype(float)
     total_supply = supply.sum()
     total_demand = demand.sum()
     extra = total_supply - total_demand
@@ -270,7 +446,7 @@ def main():
         output,total_cost = LeastCostCell(supply,demand,cost)
         method = "LeastCostCell"
     elif(sys.argv[1]=="vam"):
-        output,total_cost = VogalsApproximation(supply,demand,cost)
+        output,total_cost = VogalsApproximationV2(supply,demand,cost)
         method = "VogalsApproximation"
     else:
         print("Invalid Input");
@@ -278,6 +454,15 @@ def main():
     print("Occupied matrix of ",method,":")
     printMatrix(output)
     print("Total cost of ",method,":", total_cost)
+    print("Checking Validity of answer")
+    validity = checkAnswer(supply,demand,output)
+    if(validity):
+        print("Answer is Valid")
+    else:
+        print("Answer is Invalid")
 
 if __name__ == "__main__":
     main()
+
+end = time.time()
+print("Execution Time:",end - start)
