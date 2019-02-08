2ff4e8cd3c1198881ea9c4bd7751dd5a93ab661e
Yuvraj Garg <yuvraj97garg@gmail.com>
2019-02-09 01:52:22 +0530

    Update load.py

--- Graph/Graph Using Hash Table/lib/load.py
+++ Graph/Graph Using Hash Table/lib/load.py
@@ -35,28 +35,23 @@ def load(path):
 def load2(path):
     data=open(path,'r');    # for example: open('/home/rajesh/Desktop/HackerRank/InterviewBit/Py/SpanningTree/SpanningTreeDataset.csv','r');
     dataList=list(csv.reader(data));
-    dataArray=np.array(dataList);
+    data.close()
+    dataArray=list(dataList);
     dataList=None;
-    no_graphs=0;
-    #print(dataArray)
     for i in range(len(dataArray)):
-        #print(dataArray[i])
-        if dataArray[i][0]=='#':
-            no_graphs+=1;
-    graph_array=[[[],[]] for i in range(no_graphs)]
-    i=0;    # i is use for traversing dataArray
-    for graph_counter in range(no_graphs):
-        #print("Graph",graph_counter)
-        i+=1        # here by incrementing we reach to no of vertices cell and in next line we access no of vertices
-        temp = dataArray[i]
-        if(temp[-1] == ''): temp.pop()
-        graph_array[graph_counter][0] = temp #[int(i) for i in temp]
-        i+=1;       # now here by incrementing we reach to the edges connection so now we store edges in matrix
-        while dataArray[i][0]!='#' and dataArray[i][0]!='!':
-            #graph_array[graph_counter][1].append((int(dataArray[i][0]), int(dataArray[i][1])))
-            graph_array[graph_counter][1].append((dataArray[i][0], dataArray[i][1]))
-            i+=1;
-    return no_graphs , graph_array
-
-#path = 'C:\\Users\\Yuvraj\\Desktop\\py\\Graph\\SpanningTree\\SpanningTreeDataset2.csv'
-#no_graphs, graph_array=load2(path);
+        while(True):
+            if(dataArray[i][-1]==''):
+                dataArray[i].pop()
+                continue
+            break
+    dataArray.pop()
+    vertices = dataArray[1]
+    isWeightes = len(dataArray[2]) == 3
+    edges = []
+    if(isWeightes):
+        for i in range(2,len(dataArray)):
+            edges.append((dataArray[i][0],dataArray[i][1],int(dataArray[i][2])))
+    else:
+        for i in range(2,len(dataArray)):
+            edges.append((dataArray[i][0],dataArray[i][1]))
+    return vertices,edges
